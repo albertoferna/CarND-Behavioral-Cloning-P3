1aba5cac83022ece91c41155745b06684921554f
Alberto Fernandez <alberto@totalmar.com>
2017-03-26 00:17:14 +0100

    simplified with data_adaptation functions

--- model_LeNet.py
+++ model_LeNet.py
@@ -1,53 +1,14 @@
-import numpy as np
-import random
-import csv
-import cv2
 from keras.models import Sequential
 from keras.layers.convolutional import Convolution2D
 from keras.layers.convolutional import Cropping2D
 from keras.layers.convolutional import MaxPooling2D
 from keras.layers import Flatten, Dense, Dropout
+from data_adaptation import *
 
-lines = []
 data_folder = './data_ud/'
-with open(data_folder + 'driving_log.csv') as csv_file:
-    reader = csv.reader(csv_file)
-    for line in reader:
-        lines.append(line)
 
-images_center = []
-images_left = []
-images_right = []
-measurements = []
-
-for line in random.sample(lines[1:], 1000):
-    center_file = line[0].split('/')[-1]
-    center_image = cv2.imread(data_folder+'IMG/'+center_file.strip())
-    images_center.append(center_image)
-    measurements.append(float(line[3]))
-    flipped = cv2.flip(center_image.copy(), 1)
-    images_center.append(flipped)
-    measurements.append(-float(line[3]))
-
-    left_file = line[1].split('/')[-1]
-    left_image = cv2.imread(data_folder+'IMG/'+left_file.strip())
-    images_left.append(left_image)
-    measurements.append(float(line[3]) + 0.05)
-    flipped = cv2.flip(left_image.copy(), 1)
-    images_left.append(flipped)
-    measurements.append((-float(line[3]) + 0.05))
-
-    right_file = line[2].split('/')[-1]
-    right_image = cv2.imread(data_folder+'IMG/'+right_file.strip())
-    images_right.append(right_image)
-    measurements.append(float(line[3]) - 0.05)
-    flipped = cv2.flip(right_image.copy(), 1)
-    images_right.append(flipped)
-    measurements.append(-(float(line[3]) - 0.05))
-
-images = images_left + images_right + images_center
-X_train = np.array(images)
-y_train = measurements
+used_samples = read_data(data_folder, 1000, 20)
+X_train, y_train = get_training_data(used_samples, data_folder)
 
 # Trying to use LeNet as an inspiration
 model = Sequential()
@@ -66,16 +27,14 @@ model.add(MaxPooling2D(pool_size=(2, 2)))
 # Layer 3
 model.add(Flatten())
 model.add(Dense(120, activation='relu', kernel_initializer='TruncatedNormal'))
-model.add(Dropout(0.5))
 
 # Layer 4
 model.add(Dense(84, activation='relu', kernel_initializer='TruncatedNormal'))
-#model.add(Dropout(0.5))
 
-# Layer 6
+# Layer 5
 model.add(Dense(1, kernel_initializer='TruncatedNormal'))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, batch_size=128, validation_split=0.3, shuffle=True, epochs=15)
+model.fit(X_train, y_train, batch_size=128, validation_split=0.3, shuffle=True, epochs=10)
 
 model.save('model_LeNet.h5')
