151c1661517a4a41cc43024a1e372c1a7675ed7d
Alberto Fernandez <alberto@totalmar.com>
2017-04-10 10:54:12 +0200

    model to turn in

--- model.py
+++ model.py
@@ -11,7 +11,7 @@ from keras.callbacks import ModelCheckpoint, EarlyStopping
 from keras.layers.normalization import BatchNormalization
 from keras.regularizers import l2
 
-data_folder = '../CarND-Data-P3/data_final/'
+data_folder = '../CarND-Data-P3/data_all/'
 
 used_samples = read_data(data_folder, samples_per_bin=500, bins=200)
 X_train, y_train = get_training_data(used_samples, data_folder)
@@ -39,10 +39,10 @@ model.add(BatchNormalization())
 model.add(ELU())
 model.add(Dense(1))
 
-model.compile(optimizer="adam", loss="mse", lr=0.001)
-model.load_weights('model_commaai.h5')
+model.compile(optimizer="adam", loss="mse", lr=0.0001)
+#model.load_weights('model_commaai.h5')
 saving = ModelCheckpoint('commaai.{epoch:02d}-{val_loss:.2f}.h5', save_best_only=True)
 earlyStopping = EarlyStopping(monitor='val_loss', patience=1, verbose=0, mode='auto')
-model.fit(X_train, y_train, batch_size=32, validation_split=0.2, epochs=20, shuffle=True,
+model.fit(X_train, y_train, batch_size=64, validation_split=0.3, epochs=20, shuffle=True,
           callbacks=[saving, earlyStopping])
 model.save('model_commaai.h5')
